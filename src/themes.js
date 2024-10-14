a1197fd218e6d2cda612a59c81da35345c25de08
Bruno Bortagaray <khalisto@gmail.com>
2024-10-14 19:29:23 +0200

    feat: Apparently, I forgot to add the theme file in the last time

---
+++ src/themes.js
@@ -0,0 +1,42 @@
+export const lightTheme = {
+  colors: {
+    text: "#000",
+    sectionText: "#000",
+    backgroundStory: "#DEBD9C",
+    backgroundTransition: "#cccccc",
+    mouse: "#2D3671",
+    icon: "#5f6368",
+    button:
+      "linear-gradient(to left, #f6a192, #f6b092, #f6c492, #f6cf92, #f6d992);",
+
+    purpleLight: "#8C7FA2",
+    purple: "#D4B5E7",
+    navyBlue: "#2D3671",
+    green: "#82C752",
+    brown: "#A28E7F",
+    beige: "#DEBD9C",
+    peach: "#F6B092",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    text: "#DEBD9C",
+    sectionText: "#f3f2ee",
+    backgroundStory: "#2d2d61",
+    backgroundTransition: "#333333",
+    mouse: "#D2C98E",
+
+    button:
+      "linear-gradient(to left, #2d3671, #393983, #314daf, #0567b7, #00d4ff);",
+
+    icon: "#A09C97",
+    purpleLight: "#73805D",
+    purple: "#2B4A18",
+    navyBlue: "#D2C98E",
+    green: "#7D38AD",
+    brown: "#5d7180",
+    beige: "#214563",
+    peach: "#094f6d",
+  },
+};
